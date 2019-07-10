5341dc328e909e0c747bef22c25e245f234f60ef
hpur <hpur@liveperson.com>
2019-07-10 15:54:35 +0300

    add face screen

---
+++ KickStarter/facebookScreen.php
@@ -0,0 +1,38 @@
+<?php session_start();?>
+
+<html>
+<title>FaceAfeka</title>
+
+<head>
+<link rel="stylesheet" type="text/css" href="StyleCss/styling.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="JS/registration.js"></script>
+<script src = "JS/loginScript.js"></script>
+
+</head>
+
+<body id = "mainPageBody">
+
+	<div class="mainHeader">
+		<div class="logoName" >
+			<p id="logo"> <img src = "pics/facephoto.jpg" height="30" alt="London"> FaceAfeka</p>
+		</div>
+
+		<div class="loginDisp">
+			<div class = "loggedInUser"></div> 
+		</div>
+		
+		<div class = "logOut" >
+			<button class = "button" type = "button" name = "logOut" value = "Log Out" onclick = "logOut()">Log out</button>		
+		</div>
+	</div>
+
+
+	<div class="container">
+	</div>
+
+</body>
+</html>
+<?php
+// }
+//?>
